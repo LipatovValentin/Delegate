7bb97cf6454545aee5f7d00d691502f4b0f8bb3b
LipatovValentin <ValentinLipatov@bk.ru>
2021-03-03 20:54:36 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        public static void Test(Action<int, int, int, int, int, int, int, int> runAfterWork)
+        {
+            runAfterWork(0, 1, 2, 3, 4, 5, 6, 7);
+        }
+        static void Main(string[] args)
+        {
+            Test(delegate { Console.WriteLine("Run after work"); });
+            Console.ReadKey();
+        }
+    }
+}
